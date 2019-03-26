479993911b9dcced00909d166f6f786516597ff8
Sergey Stepanets <stepanetssergey@gmail.com>
2019-03-26 17:18:33 +0200

    change deploy js and test

--- migrations/2_TokenKCYCrowdsale.js
+++ migrations/2_TokenKCYCrowdsale.js
@@ -1,10 +1,13 @@
 token = artifacts.require("./tokens/token.sol");
+sale = artifacts.require("./tokensale/sale.sol");
 
 
 module.exports = async deployer => {
   var accounts =  await web3.eth.getAccounts();
   var owner = accounts[0];
   console.log("Token owner: ",owner);
-  await deployer.deploy(token,"Angel","ANG",18,{from: owner})
-  console.log("ANG token address:" + token.address);
+  await deployer.deploy(token,"Angel","ANG",18,{from: owner});
+  await deployer.deploy(sale,{from: owner});
+  console.log("Sale tokens address:"+sale.address);
+//  console.log("ANG token address:" + token.address);
 }
--- truffle.js
+++ truffle.js
@@ -8,6 +8,11 @@ module.exports = {
        host: '127.0.0.1',
        port: 8545,
        network_id: '*' ,// Match any network id
-     }
+     },
+  },
+  solc: {
+   optimizer: {
+     enabled: true,
+     runs: 200}
    }
 };
