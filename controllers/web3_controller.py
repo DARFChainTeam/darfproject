e0da0d322520fdf3851084244f10e03bbaddd11d
stanta <stanislav@taktaev.com>
2019-04-30 13:51:31 +0300

    added structure of project

--- controllers/web3_controller.py
+++ controllers/web3_controller.py
@@ -6,9 +6,35 @@
 #workflow   https://www.draw.io/#G1yXU5Wpk7tX8-46XPNUB-CUb27HRQmbiy
 
 
+
 import json
 import web3
 
 from web3 import Web3, HTTPProvider, TestRPCProvider
 from solc import compile_source
 from web3.contract import ConciseContract
+
+class web3_controller:
+
+    def ProjectStart(self):
+        return
+    def ChangeProjectInfo(self):
+        return
+    def  FinishProject  (self):
+        return
+    def ProjectAddState(self):
+        return
+
+    def ProjectGet_state(self):
+        return
+
+    def SetRights(self):
+        return
+    def CheckRights(self):
+        return
+
+# token management
+#todo add token management to menu, views
+
+
+
